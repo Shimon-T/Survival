aee4c33becf4e0be64e52be7047ea13cad81b1b2
Shimon-Tanaka <shimontk11165478@gmail.com>
2025-06-01 15:36:38 +0900

    Initial Commit

---
+++ ServivalGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ServivalGame
+//
+//  Created by 田中志門 on 6/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ServivalGame/ServivalGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServivalGameApp.swift
+//  ServivalGame
+//
+//  Created by 田中志門 on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ServivalGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
